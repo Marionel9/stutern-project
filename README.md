36417b8bb49c1909a36730df741811b8f125cf8e
Marionel9 <nenemarionel1@gmail.com>
2023-06-07 17:23:21 +0100

    Changes to be committed: 	deleted:    Data types quick review.ipynb 	modified:   README.md 	deleted:    hello.py 	modified:   programme.ipynb

---
+++ README.md
@@ -0,0 +1,5 @@
+### Final Project
+
+### Task
+
+Your task will be to extract, import and analyze data from IBM Employee Attrition database
--- hello.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-
-c = conn.cursor()
-
-#title and pages are columns and books is the table name
-c.execute('''CREATE TABLE IF NOT EXISTS books 
-            (title TEXT, pages INTEGER)''')
